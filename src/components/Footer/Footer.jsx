2367aaa3b63fefd0d6d7510481773cff9d3b5099
AndreaMartinezRamos <andrea.martinez.ramos@hotmail.com>
2024-01-16 23:55:18 +0100

    add css provisional header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,18 +4,10 @@ const Footer = () =>{
     return(
         <div className="footer">
             <div>
-                <h3>I need up</h3>
-                <ul>
-                    <li><a href="/faqs">FAQs</a></li>
-                    <li><a href="/demands">Demands</a></li>
-                    <li><a href="/">Categories</a></li> {/* Si se hace una página para categorias */}
-                    <li><a href="/">Freelancers</a></li>{/* Si se hace una página que saque usuarios */}
-                </ul>
-            </div>
-            <div>
                 <h3>About</h3>
                 <ul>
                     <li><a href="">About us</a></li>
+                    <li><a href="/">Categories</a></li> {/* Si se hace una página para categorias */}
                     <li><a href="">Contact</a></li>
                     <li><a href="">Starting guide</a></li>
                     <li><a href="">Where all started</a></li>
