e34ff7d0da0391771122bc3f4297b5a84ecf641b
i-am-pepsiman <mani-designs@hotmail.com>
2024-01-17 20:36:32 +0100

    Added headers

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <img src="../logo.jpg"/>
+      <h1>Needs portal</h1>
+      <nav>
+        <Link to="/" className='links'>Home</Link>
+        <Link to="/demands" className='links'>Demands</Link>
+        <Link to="/faqs" className='links'>FAQs</Link>
+        <Link to="/login" className='links'>Login</Link>
+        <Link to="/signup" className='links'>Sign up</Link>
+      </nav>
+    </header>
+  )
+}
+//CAMBIOS
+
+export default Header
