f9b783965f32678f26b979a4704b382c7c641608
Abraham <228.abraham.diaz@gmail.com>
2024-01-20 11:30:36 +0000

    añadimos contact y about

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+import './Footer.css'
+const Footer = () => {
+
+    return (
+        <div>
+            <div className='about'>
+                <h3>About</h3>
+                <ul>
+                    <li><Link href="/about">About us</Link></li>
+                    <li><Link href="/">Categories</Link></li> {/* Si se hace una página para categorias */}
+                    <li><Link href="/contact">Contact</Link></li>
+                    <li><Link href="">Starting guide</Link></li>
+                    <li><Link href="https://www.hackaboss.com">Where all started</Link></li>
+                </ul>
+            </div>
+            <div className='terms'>
+                <h3>Terms</h3>
+                <ul>
+                    <li><Link href="/privacy">Privacy Policy</Link></li>
+                    <li><Link href="/terms">Terms and Conditions</Link></li>
+                    <li><Link href="/copyright">Copyright Policy</Link></li>
+                </ul>
+            </div>
+            <p className='copyright'>Copyright © 2024 I need up. All rights reserved.</p>
+        </div>
+    )
+}
+export default Footer
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
 import Browser from "../../components/Browser/Browser";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Info from "../../components/Info/info";
 
-const Home = () =>{
+const Home = () => {
 
-    return(
+    return (
         <div>
-            <Header/>
-            <Browser/>
-            <Info/>
+            <Header />
+            <Browser />
+            <Info />
+            <Footer />
         </div>
     )
 }
