984af7da30ab010cdc4573f2ca41793ce95bdc58
i-am-pepsiman <mani-designs@hotmail.com>
2024-01-20 03:18:05 +0100

    Added various styles to some components, not finished though

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,19 @@ import './Header.css'
 const Header = () => {
 
   return (
-    <header>
+    <div>
       <Topbar />
-      <h1>Needs portal</h1>
-      <nav>
-        <Link to="/" className='links'>Home</Link>
-        <Link to="/demands" className='links'>Demands</Link>
-        <Link to="/faqs" className='links'>FAQs</Link>
-        <Link to="/login-signup" className='links'>Login/Signup</Link>
-      </nav>
-    </header>
+        <header>
+          <h1>iNeed<span>Up</span></h1>
+          <h3>Needs and solutions connected</h3>
+          <ul className='links_container'>
+            <li><a href='/'>HOME</a></li>
+            <li><a href='/demands'>DEMANDS</a></li>
+            <li><a href='/faqs'>FAQS</a></li>
+            <li><a href='/login-signup'>LOGIN/SIGNUP</a></li>
+          </ul>
+        </header>
+      </div>
   )
 }
 
--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -2,16 +2,20 @@ import './Info.css'
 const Info = () =>{
 
     return(
-        <div>
-            <aside>Platform information and updates</aside>
-            <aside className='asideUsers'>
-                <p>Featured users of the week:</p>
-                <ul>
-                    <li>User 1</li>
-                    <li>User 2</li>
-                    <li>User 3</li>
-                </ul>
-            </aside>
+        <div className='flex_container'>
+            <div className='flex_container_child'>
+                <h3>Platform information and updates:</h3>
+            </div>
+            <div className='flex_container_child'>
+                <aside className='asideUsers'>
+                    <h3>Featured users of the week:</h3>
+                    <ul>
+                        <li>User 1</li>
+                        <li>User 2</li>
+                        <li>User 3</li>
+                    </ul>
+                </aside>
+            </div>
         </div>
     )
 }
