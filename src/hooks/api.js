7a6e541920fac1ed0fda2973d950abda56b4334a
Abraham <228.abraham.diaz@gmail.com>
2024-01-24 17:10:43 +0000

    añadimos data a api.js

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -7,9 +7,13 @@ const apiHost = 'http://localhost:8080/';
 export const useDemandsAlls = () => useFetch(apiHost + 'demands')
 export const useDemands = (query) => useFetch('demands?' + new URLSearchParams(query).toString())
 export const useDemand = (id) => useFetch(apiHost + `demands/${id}`)
-
+export const useProposalByDemands = (id) => useFetch(apiHost + `demands/${id}` + '/proposals')
+export const useDeleteDemands = (id) => useFetchPost(apiHost + `demands/${id}`)
 //----------------------------------------------------proposals-------------------------------
-
+export const useNewProposal = (id) => useFetchPost(apiHost + `/proposals/${id}`)
+export const useEditProposal = (id) => useFetchPost(apiHost + `/proposals/${id}`)
+export const useDeleteProposal = (id) => useFetchPost(apiHost + `/proposals/${id}`)
+export const useUpdateStatusProposal = (id) => useFetchPost(apiHost + `/proposals/${id}` + '/updateStatus')
 
 //---------------------------------------------------user-------------------------------------
 export const useLogin = () => useFetchPost(apiHost + 'user/login')
@@ -19,12 +23,12 @@ export const UseRegister = () => useFetchPost(apiHost + 'users')
 //---------------------------------------------------votes-------------------------------------
 
 export const useUserActions = () => {
-    const fetchPost = useFetchPost()
-    return {
-      login: (email, password) => fetchPost(apiHost + '/users/login', { email, password }),
-      createEntry: (title, place, description) => fetchPost(apiHost + '/entries', { title, place, description }),
-      removeEntry: (id) => fetchPost(apiHost + '/entries/' + id, null, 'DELETE'),
-      addPhoto: (id, fd) => fetchPost(apiHost + '/entries/' + id + '/photos', fd),
-      vote: (id, value) => fetchPost(apiHost + '/proposals/' + id + '/votes', { value })
-    }
+  const fetchPost = useFetchPost()
+  return {
+    login: (email, password) => fetchPost(apiHost + '/users/login', { email, password }),
+    createEntry: (title, place, description) => fetchPost(apiHost + '/entries', { title, place, description }),
+    removeEntry: (id) => fetchPost(apiHost + '/entries/' + id, null, 'DELETE'),
+    addPhoto: (id, fd) => fetchPost(apiHost + '/entries/' + id + '/photos', fd),
+    vote: (id, value) => fetchPost(apiHost + '/proposals/' + id + '/votes', { value })
   }
+}
