46d609872266143fd608939bffa159f816ca1f44
Abraham <228.abraham.diaz@gmail.com>
2024-01-30 17:24:52 +0000

    añadimos errores si no ay proposal

--- src/components/Proposal/Proposals.jsx
+++ src/components/Proposal/Proposals.jsx
@@ -5,13 +5,20 @@ import { useProposalByDemands } from "../../hooks/api";
 const Proposals = () => {
     const { id } = useParams();
     const proposalsData = useProposalByDemands(id);
+    if (proposalsData.error) {
+        return (
+            <div>
+                <p>This demand has no proposals.</p>
+            </div>
+        )
+    }
     return (
         <div>
-            {proposalsData && proposalsData?.data.proposals.map((p) => (
+            {proposalsData ? proposalsData?.data.proposals.map((p) => (
                 <div key={p.id}>
                     <p>{p.title}</p>
                 </div>
-            ))}
+            )) : ''}
         </div>
     );
 }
