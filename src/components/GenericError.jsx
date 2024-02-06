42ce30df80a633232d0c66347df0bfbb420dd7d9
AndreaMartinezRamos <andrea.martinez.ramos@hotmail.com>
2024-02-06 23:58:34 +0100

    error css

--- src/components/GenericError.jsx
+++ src/components/GenericError.jsx
@@ -1,10 +1,14 @@
+import './Error.css'
 const GenericError = () => {
   const reload = () => window.location.reload()
   return (
-    <div>
-      <h1>¡Algo ha salido mal!</h1>
-      <p>Vaya, esto es embarazoso. La página ha fallado.</p>
-      <p>¿Por qué no pruebas a <a onClick={reload}>recargar</a>?</p>
+    <div className="error_container">
+      <div className='error_content'>
+        <h1 className='error_title'>¡Algo ha salido mal!</h1>
+        <p>Vaya, esto es embarazoso. La página ha fallado.</p>
+        <p>¿Por qué no pruebas a <a className='reload' onClick={reload}>recargar</a>?</p>
+        <img src='src\assets\images\error_animation.gif'/>
+      </div>
     </div>
   )
 }
