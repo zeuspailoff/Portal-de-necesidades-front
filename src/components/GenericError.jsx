9e1874dd7c299b5fcadd5c7c9cc2c55ee96e9c6c
Abraham <228.abraham.diaz@gmail.com>
2024-02-08 13:16:18 +0000

    ajustes al error boundary

--- src/components/GenericError.jsx
+++ src/components/GenericError.jsx
@@ -1,13 +1,19 @@
+import { useNavigate } from 'react-router-dom'
 import './Error.css'
 const GenericError = () => {
-  const reload = () => window.location.reload()
+  const navigate = useNavigate()
+  const reload = () => {
+    navigate('/')
+    window.location.reload()
+  }
+
   return (
     <div className="error_container">
       <div className='error_content'>
         <h1 className='error_title'>Something's gone wrong!</h1>
         <p>Oops, this is embarrassing. The page has failed.</p>
         <p>Why don't you try <a className='reload' onClick={reload}>reload</a>?</p>
-        <img src='src\assets\images\error_animation.gif'/>
+        <img src='src\assets\images\error_animation.gif' />
       </div>
     </div>
   )
