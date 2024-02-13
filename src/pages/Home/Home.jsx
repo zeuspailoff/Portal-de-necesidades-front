63e551c309d33b5206fc6c8cffb747b72427e343
Abraham <228.abraham.diaz@gmail.com>
2024-02-13 16:07:51 +0000

    Co-authored-by: TesseractedBallsax <i-am-pepsiman@users.noreply.github.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Info from "../../components/Info/info";
 import './Home.css'
+import Logo from "../../components/Logo/logo.png";
 const Home = () => {
 
     return (
         <div className="general-container">
             <div className="home_description_container">
-                <p className="home_description">Welcome to <strong>iNeedUp</strong>, a website where you can make and find demands and make proposals. Explore our <a href="/categories">categories</a> to see the demands of your interest</p>
+                <p className="home_description">Welcome to <img className='logo-description' src={Logo} />
+                    , a website where you can make and find demands and make proposals. Explore our <a href="/categories">categories</a> to see the demands of your interest</p>
             </div>
             <Info />
         </div>
