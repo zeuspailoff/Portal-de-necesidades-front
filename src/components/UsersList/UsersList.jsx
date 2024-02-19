2cb22b117417fb1aa80dcab792e6065d0b4585fb
zeuspailoff <228.abraham.diaz@gmail.com>
2024-02-19 14:00:07 +0000

    ERROR DE USERS

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -7,13 +7,13 @@ const UserList = () => {
   const usersData = useUserList()
   const [users] = useState(usersData?.data?.users?.slice(0, 10))
   const apiUrl = import.meta.env.VITE_BACKEND_URL;
-
+  console.log(users)
   return (
     <div className="userlist_container">
       <ul className="userList">
-        {users.map(u => (
+        {users?.map(u => (
           <div className="single_user_info" key={u.id}>
-            {u && u.profile_picture ? (
+            {u && u?.profile_picture ? (
               <img className="avatarHome" src={`${apiUrl}${u.profile_picture.replace(/\\/g, '/')}`} alt={u.username} />
             ) : <img className="avatarHome" src={'https://w7.pngwing.com/pngs/77/140/png-transparent-training-needs-analysis-needs-assessment-needs-analysis-orange-logo-need.png'} alt={u.username} />}
             <Link to={`/profile/${u.id}`}> {u && u.username} </Link>
