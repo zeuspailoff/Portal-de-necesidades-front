5e9c778de030f5f307ff9e22e60282e5e430eea9
Abraham <228.abraham.diaz@gmail.com>
2024-03-20 16:35:56 +0000

    Co-authored-by: keviinmiichael <keviinmiichael@users.noreply.github.com>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,36 @@
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
-
-const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Demands', href: '/demands', current: false },
-  { name: 'FAQS', href: '/faqs', current: false },
-];
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useUser } from '../../UserContext';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
 const Header = () => {
+  const [user, setUser] = useUser();
+  const navigate = useNavigate()
+  const navigation = [
+    { name: 'Home', href: '/', current: useLocation().pathname == '/' ? true : false },
+    { name: 'Demands', href: '/demands', current: useLocation().pathname == '/demands' ? true : false },
+    { name: 'FAQS', href: '/faqs', current: useLocation().pathname == '/faqs' ? true : false },
+  ];
+
+  const apiUrl = import.meta.env.VITE_BACKEND_URL;
+  const backgroundImageUrl = user && user.profile_picture ? `${apiUrl}/${user.profile_picture.replace(/\\/g, '/')}` : 'none'
+
+  const logout = () => {
+    setUser();
+    navigate('/');
+  }
+
   return (
-    <Disclosure as="nav" className="bg-slate-300">
+    <Disclosure as="nav" className="bg-gray-500">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7x2 px-2 sm:px-6 lg:px-9">
-            <div className="flex h-16 items-center justify-center"> {/* Ajusta la alineación de los elementos al centro */}
+          <div className="mx-auto max-w-7x2 px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -32,22 +43,22 @@ const Header = () => {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 justify-items-start sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 ">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
                     src="../src\components\Logo\logo.png"
                     alt="Your Company"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block justify-center">
-                  <div className="flex space-x-5 justify-center">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <Link
                         key={item.name}
                         to={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-400 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -59,16 +70,23 @@ const Header = () => {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
+                <Link
+                  to={"/demands/new"}
+                  className={useLocation().pathname == '/demands/new' ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'}
+                >
+                  New Demand
+                </Link>
+
+                {!user && <Link to="/login" className={useLocation().pathname == '/login' ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'} > Login</Link>}
+                {user && <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        className="h-14 w-14 rounded-full"
+                        src={backgroundImageUrl}
+                        alt="imagen profile"
                       />
                     </Menu.Button>
                   </div>
@@ -84,28 +102,27 @@ const Header = () => {
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to={"/profile/" + user.id}
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            aria-current={'page'}
                           >
-                            Your Profile
-                          </a>
+                            Profile
+                          </Link>
                         )}
                       </Menu.Item>
-
                       <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
+                        <a
+                          href="#"
+                          className={classNames('block px-4 py-2 text-sm text-gray-700')}
+                          onClick={logout}
+                        >
+                          Sign out
+                        </a>
                       </Menu.Item>
                     </Menu.Items>
                   </Transition>
-                </Menu>
+                </Menu>}
               </div>
             </div>
           </div>
@@ -129,8 +146,9 @@ const Header = () => {
             </div>
           </Disclosure.Panel>
         </>
-      )}
-    </Disclosure>
+      )
+      }
+    </Disclosure >
   );
 };
 
