daf287c2aaf7b5518efabee128bf6648c6f4b2e8
Abraham <228.abraham.diaz@gmail.com>
2024-04-24 00:15:58 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
           <FloatingButton />
           <Footer />
         </Suspense>
-          <div id="modal-root"></div>
+        <div id="modal-root"></div>
       </ErrorBoundary>
     </>
   )
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -5,28 +5,28 @@ import Logo from '../Logo/logo.png';
 
 
 const Header = () => {
-  return (
-    <div className='header_wrapper'>
-      <div className='preview_card_container_hidden'>
-        <ProfilePreviewCard />
-      </div>
+    return (
+        <div className='header_wrapper'>
+            <div className='preview_card_container_hidden'>
+                <ProfilePreviewCard />
+            </div>
 
-      <div className='logo_container'>
-        <Link to='/' style={{display:'flex'}}><img className='logo' src={Logo} /></Link>
-      </div>
+            <div className='logo_container'>
+                <Link to='/' style={{ display: 'flex' }}><img className='logo' src={Logo} /></Link>
+            </div>
 
-      <div className='links_wrapper'>
-        <ul className='links_container'>
-          <li><Link to='/'>HOME</Link></li>
-          <li><Link to='/demands'>DEMANDS</Link></li>
-          <li><Link to='/faqs'>FAQS</Link></li>
-        </ul>
-      </div>
-      <div className='preview_card_container'>
-        <ProfilePreviewCard />
-      </div>
-    </div>
-  )
+            <div className='links_wrapper'>
+                <ul className='links_container'>
+                    <li><Link to='/'>HOME</Link></li>
+                    <li><Link to='/demands'>DEMANDS</Link></li>
+                    <li><Link to='/faqs'>FAQS</Link></li>
+                </ul>
+            </div>
+            <div className='preview_card_container'>
+                <ProfilePreviewCard />
+            </div>
+        </div>
+    )
 }
 
 export default Header
